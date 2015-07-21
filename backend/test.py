6aab72ca7cbd74f29fa19612a317d722ec524177
Sergii boonya Buinytskyi <boonya41@gmail.com>
2015-07-21 23:57:06 +0300

    Revert "Move test package to the root of project."
    This reverts commit 5db82e18386d1a5c62b9080fb22c45b4689d646f.

--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 __author__ = 'boonya'
 import unittest
-from tests.controllers.post import CtrlPostTestCase
-from tests.models.post import ModelPostTestCase
+from app.tests.controllers.post import CtrlPostTestCase
+from app.tests.models.post import ModelPostTestCase
 
 if __name__ == '__main__':
     unittest.main()

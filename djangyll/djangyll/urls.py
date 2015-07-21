22a7ff94487c03822eaa518d781b9125a61faac9
Sergii boonya Buinytskyi <boonya41@gmail.com>
2015-07-21 22:02:58 +0300

    Django code was removed

--- djangyll/djangyll/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^post/?', include('apps.post.urls'))
-]

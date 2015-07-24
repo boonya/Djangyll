30507b976f1022c6b79708426a2363a7b738b596
Sergii boonya Buinytskyi <boonya41@gmail.com>
2015-07-24 22:21:35 +0300

    Tests directory was moved

--- backend/test.py
+++ backend/test.py
@@ -4,8 +4,8 @@ import unittest
 import logging
 import sys
 
-from app.tests.controllers.post import CtrlPostTestCase
-from app.tests.models.post import ModelPostTestCase
+from tests.controllers.post import CtrlPostTestCase
+from tests.models.post import ModelPostTestCase
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr)
